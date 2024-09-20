26070241c9f8a840798ca5784cb0844c85a4e79e
bntxperses <dbikramyt@gmail.com>
2024-09-20 11:17:21 +0530

    Alioth: Let there be light

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_alioth.mk
+    $(LOCAL_DIR)/genesis_alioth.mk
+
+COMMON_LUNCH_CHOICES := \
+    genesis_alioth-user \
+    genesis_alioth-userdebug \
+    genesis_alioth-eng
