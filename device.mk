d151e90589a8840cb04d02eb39d7e9ea2db299ab
Iam-PAIN <mrpainhere@gmail.com>
2024-09-27 21:21:18 +0530

    Alioth: Convert Overlays to RRO

--- device.mk
+++ device.mk
@@ -46,9 +46,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/st_license.lic:$(TARGET_COPY_OUT_VENDOR)/etc/camera/st_license.lic
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+PRODUCT_PACKAGES += \
+    AliothFrameworksOverlay \
+    AliothSystemUIOverlay \
+    AliothGenesisSystemUIOverlay
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
